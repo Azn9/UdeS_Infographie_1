ac8c98c6ca3e1539d4721845d258b2fd3d66186e
Ugo-Larsonneur <ugo.larsonneur0@gmail.com>
2023-10-31 12:10:19 -0400

    Added Utilities

--- PM3DApi/Api/Private/Util/Util.cpp
+++ PM3DApi/Api/Private/Util/Util.cpp
@@ -8,3 +8,23 @@ DirectX::XMFLOAT3 Util::Lerp(DirectX::XMFLOAT3 a, DirectX::XMFLOAT3 b, float t)
 		a.z + (b.z - a.z) * t
 	};
 }
+
+DirectX::XMFLOAT2 Util::operator*(const DirectX::XMFLOAT2& a, const float& f)
+{
+	return {a.x * f, a.y * f};
+}
+
+DirectX::XMFLOAT2 Util::operator/(const DirectX::XMFLOAT2& a, const float& f)
+{
+	return {a.x / f, a.y / f};
+}
+
+DirectX::XMFLOAT2 Util::operator+(const DirectX::XMFLOAT2& a, const DirectX::XMFLOAT2& b)
+{
+	return {a.x + b.x, a.y + b.y};
+}
+
+DirectX::XMFLOAT2 Util::operator-(const DirectX::XMFLOAT2& a, const DirectX::XMFLOAT2& b)
+{
+	return {a.x - b.x, a.y - b.y};
+}
--- PM3DApi/Api/Public/Util/Util.h
+++ PM3DApi/Api/Public/Util/Util.h
@@ -2,10 +2,12 @@
 
 #include <DirectXMath.h>
 
-class Util
+namespace Util
 {
-public:
-	static DirectX::XMFLOAT3 Lerp(DirectX::XMFLOAT3 a, DirectX::XMFLOAT3 b, float t);
-
+	DirectX::XMFLOAT3 Lerp(DirectX::XMFLOAT3 a, DirectX::XMFLOAT3 b, float t);
+	DirectX::XMFLOAT2 operator+(const DirectX::XMFLOAT2& a, const DirectX::XMFLOAT2& b);
+	DirectX::XMFLOAT2 operator-(const DirectX::XMFLOAT2& a, const DirectX::XMFLOAT2& b);
+	DirectX::XMFLOAT2 operator*(const DirectX::XMFLOAT2& a, const float& f);
+	DirectX::XMFLOAT2 operator/(const DirectX::XMFLOAT2& a, const float& f);
 };
 
