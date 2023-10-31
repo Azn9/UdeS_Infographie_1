e200a241e55ab2f3b66a01a89e43134270f32179
Axel JOLY <azn9.pro@gmail.com>
2023-10-30 22:00:38 -0400

    feat: event system

---
+++ PM3DApi/Api/Public/EventSystem/Basic/WindowResizeEvent.h
@@ -0,0 +1,17 @@
+#pragma once
+
+namespace PM3D_API
+{
+class WindowResizeEvent final : public Event
+{
+public:
+	WindowResizeEvent(const int width, const int height) : Event(), width(width), height(height) {}
+
+	[[nodiscard]] int GetWidth() const { return width; }
+	[[nodiscard]] int GetHeight() const { return height; }
+
+private:
+	int width;
+	int height;
+};
+}
